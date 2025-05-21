0d28308c3356c3d6a42f294177faafac0e742746
LILOBONdev <naly87267@gmail.com>
2025-05-21 19:26:30 +0500

    Update start.py

--- start.py
+++ start.py
@@ -4,5 +4,6 @@ from utils.logo import logo
 
 print(Fore.RED + logo + Fore.WHITE)
 while True:
+    os.system('title DISCORD TOKEN CHECKER')
     input('[+] Enter to continue BRO')
     _ = asyncio.run(main())
